c9119031f0b57f0406053bc5f9e21425b22254fe
KYPoseidonL <1191815656@qq.com>
2019-10-19 18:29:56 +0800

    xcode11 build 4

--- Pods/Target Support Files/Pods-KYResearchCode/Pods-KYResearchCode-resources.sh
+++ Pods/Target Support Files/Pods-KYResearchCode/Pods-KYResearchCode-resources.sh
@@ -92,12 +92,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/IQKeyboardManager/IQKeyBoardManager/Resources/IQKeyboardManager.bundle"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
   install_resource "${PODS_ROOT}/SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/IQKeyboardManager/IQKeyBoardManager/Resources/IQKeyboardManager.bundle"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
   install_resource "${PODS_ROOT}/SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
 fi
